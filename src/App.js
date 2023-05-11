05792f4422bb776b62d1b36d92b6ff0c86558e3b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-11 17:31:16 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Welcome to my website.
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Login</a></li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <h1>Welcome to my website</h1>
+        <section>
+          <h2>Login</h2>
+          <form>
+            <label>
+              Username:
+              <input type="text" />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input type="password" />
+            </label>
+            <br />
+            <button type="submit">Login</button>
+          </form>
+        </section>
+      </main>
     </div>
   );
 }
 
 export default App;
+
